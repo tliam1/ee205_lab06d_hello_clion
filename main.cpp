70474e5a8bbd49f4fb091f69dd1fb809ab0f219c
tliam1 <tliam@hawaii.edu>
2022-02-22 17:22:56 -1000

    Updated with my personal info

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main() {
    std::cout << "Hello, University of Hawaii, College of Engineering!" << std::endl;
    std::cout << "Lab 06d - Hello CLion - EE 205 - Spr 2022" << std::endl;
    std::cout << std::endl;
-   std::cout << "My name is:  <<YOUR NAME HERE>>" << std::endl;
-   std::cout << "A fun fact about me is <<TELL ME HOW MUCH YOU LOVE CATS>>" << std::endl;
+   std::cout << "My name is:  Liam Tapper" << std::endl;
+   std::cout << "A fun fact about me is I am a dog person" << std::endl;
 
    return 0;
 }
